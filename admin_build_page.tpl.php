e3c5e39ea32a53962a4cb66fa3923f3cfcef5022
Kerfred <Kerfred@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-29 20:18:38 +0000

    Update admin page
    git-svn-id: https://plugins.svn.wordpress.org/fg-joomla-to-wordpress/trunk@631967 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin_build_page.tpl.php
+++ admin_build_page.tpl.php
@@ -127,6 +127,7 @@
 			<li><?php _e('Joomla 2.5 featured images', 'fgj2wp'); ?></li>
 			<li><?php _e('Mambo 4.5 and 4.6 compatible', 'fgj2wp'); ?></li>
 			<li><?php _e('K2 content', 'fgj2wp'); ?></li>
+			<li><?php _e('Flexicontent content', 'fgj2wp'); ?></li>
 		</ul>
 		<div style="text-align: center;">
 			<a href="http://www.fredericgilles.net/fg-joomla-to-wordpress/" target="_blank"><img src="http://www.fredericgilles.net/wp-content/uploads/premium-version.png" alt="Buy Premium Version" /></a>
