d5f23533042976f975ad0768a915995d6e1b5932
Kerfred <Kerfred@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 19:59:00 +0000

    Tested with WordPress 4.2.2
    git-svn-id: https://plugins.svn.wordpress.org/fg-joomla-to-wordpress/trunk@1156466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-fg-joomla-to-wordpress.php
+++ includes/class-fg-joomla-to-wordpress.php
@@ -69,7 +69,7 @@ class FG_Joomla_to_WordPress {
 	public function __construct() {
 
 		$this->plugin_name = 'fgj2wp';
-		$this->version = '2.1.2';
+		$this->version = '2.2.0';
 
 		$this->load_dependencies();
 		$this->set_locale();
